7545c2aba5a8c3d2b90e9088aba726f629c3a306
KolyoKolev <kolyo.yovchev.kolev@gmail.com>
2021-04-21 12:05:28 +0300

    add selectors abstraction

---
+++ test/selectors/index.js
@@ -0,0 +1,9 @@
+export const HEROUAPP_LOGIN_PAGE = {
+  USERNAME_FIELD: '#username',
+  USERNAME_VALUE: 'tomsmith',
+  PASSWORD_FIELD: '#password',
+  PASSWORD_VALUE: 'SuperSecretPassword!',
+  SUBMIT_BTN: 'button[type="submit"]',
+  FLASH: '#flash',
+  FLASH_TEXT: 'You logged into a secure area!',
+};
--- test/specs/herouapp-login-page.spec.js
+++ test/specs/herouapp-login-page.spec.js
@@ -1,16 +1,21 @@
 import { URLS } from '../pages/';
+import { HEROUAPP_LOGIN_PAGE } from '../selectors';
 
 describe('My Login application', () => {
   it('should login with valid credentials', async () => {
     await browser.url(URLS.HEROKUAPP_LOGIN_PAGE);
 
-    await (await $('#username')).setValue('tomsmith');
-    await (await $('#password')).setValue('SuperSecretPassword!');
-    await (await $('button[type="submit"]')).click();
+    await (await $(HEROUAPP_LOGIN_PAGE.USERNAME_FIELD)).setValue(
+      HEROUAPP_LOGIN_PAGE.USERNAME_VALUE
+    );
+    await (await $(HEROUAPP_LOGIN_PAGE.PASSWORD_FIELD)).setValue(
+      HEROUAPP_LOGIN_PAGE.PASSWORD_VALUE
+    );
+    await (await $(HEROUAPP_LOGIN_PAGE.SUBMIT_BTN)).click();
 
-    await expect($('#flash')).toBeExisting();
-    await expect($('#flash')).toHaveTextContaining(
-      'You logged into a secure area!'
+    await expect($(HEROUAPP_LOGIN_PAGE.FLASH)).toBeExisting();
+    await expect($(HEROUAPP_LOGIN_PAGE.FLASH)).toHaveTextContaining(
+      HEROUAPP_LOGIN_PAGE.FLASH_TEXT
     );
   });
 });
